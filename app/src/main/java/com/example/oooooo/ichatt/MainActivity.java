845cd8b603fb514b2b01f833e94d3198e8e522a4
zhangleicha <903664613@qq.com>
2017-03-22 18:02:23 +0800

    pro

--- app/src/main/java/com/example/oooooo/ichatt/MainActivity.java
+++ app/src/main/java/com/example/oooooo/ichatt/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.oooooo.ichatt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EMClient.getInstance().login("zhanglei","1211",new EMCallBack() {//回调
+            @Override
+            public void onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+                Log.e("main", "登录聊天服务器成功！");
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                Log.e("main", "登录聊天服务器失败！");
+            }
+        });
     }
 }
